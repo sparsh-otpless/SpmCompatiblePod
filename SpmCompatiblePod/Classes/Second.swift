4d98d88621d52328594a1591b6b29ecd8f32fdaa
sparsh-otpless <sparsh.chadha@otpless.com>
2024-05-24 17:33:41 +0530

    new file

---
+++ SpmCompatiblePod/Classes/Second.swift
@@ -0,0 +1,14 @@
+//
+//  Second.swift
+//  SpmCompatiblePod
+//
+//  Created by Sparsh on 24/05/24.
+//
+
+import Foundation
+
+@objc public class Second: NSObject {
+    public func greetSecond() {
+        print("Bye bye")
+    }
+}
